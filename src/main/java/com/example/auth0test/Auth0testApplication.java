7c04f34a0ad809ec11f94a4bcc5ef91f807d92f0
francosantzz <franco.santibanez@alumnos.frm.utn.edu.ar>
2024-05-30 14:14:11 -0300

    subo back

---
+++ src/main/java/com/example/auth0test/Auth0testApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth0test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth0testApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auth0test/Auth0testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth0test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0testApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
